e32e1fa3fccda33ac3d449640594678ced75b50d
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2025-03-07 23:04:56 +0000

    Add 1141. User Activity for the Past 30 Days I
    Signed-off-by: Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>

---
+++ Easy/1141_user_activity_for_the_past_30_days_i.sql
@@ -0,0 +1,25 @@
+-- Problem: 1141. User Activity for the Past 30 Days I
+
+-- Description:
+-- Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
+-- A user was active on someday if they made at least one activity on that day.
+
+-- Return the result table in any order.
+
+-- Tables:
+-- Activity:
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | session_id    | int     |
+-- | activity_date | date    |
+-- | activity_type | enum    |
+-- +---------------+---------+
+
+-- Solution:
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY activity_date;
