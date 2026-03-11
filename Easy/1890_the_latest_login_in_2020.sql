a07f63d9b1cb2ae146417fbc1e8f53d5a5b824a5
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2026-03-11 12:12:12 +0000

    Add 1890. The Latest Login in 2020
    Signed-off-by: Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>

---
+++ Easy/1890_the_latest_login_in_2020.sql
@@ -0,0 +1,22 @@
+-- Problem: 1890. The Latest Login in 2020
+
+-- Tables:
+-- Logins:
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+
+-- Description: Write a solution to report the latest login for all users in the year 2020.
+-- Do not include the users who did not login in 2020.
+
+-- Return the result table in any order.
+
+-- Solution:
+
+SELECT user_id, MAX(time_stamp) AS last_stamp
+FROM Logins
+WHERE YEAR(time_stamp) = 2020
+GROUP BY user_id;
